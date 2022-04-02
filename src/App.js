036b8420ee2e613c00322e1e13a23812542c3d96
DiegoVillasenorVilla <divivi.619@gmail.com>
2022-04-02 14:53:03 -0600

    I add the API_URL with my key and useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import {useEffect} from 'react';
 import './App.css';
 
-function App() {
+//API key: 316b61d2
+
+const API_URL = "http://www.omdbapi.com/?apikey=316b61d2";
+
+const App = () => {
+
+    const searchMovies = async (title) =>  {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+  
+        console.log(data)
+    }
+
+  useEffect(()=>{
+    searchMovies('Batman')
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
     </div>
   );
 }
