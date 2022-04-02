3c6f7f91f314a11e9bb3493cecdf21fa68451af1
DiegoVillasenorVilla <divivi.619@gmail.com>
2022-04-02 15:56:32 -0600

    I add App.css, search.svg and a example of a movie

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import {useEffect} from 'react';
 import './App.css';
+import SearchIcon from "./search.svg"
 
 //API key: 316b61d2
 
 const API_URL = "http://www.omdbapi.com/?apikey=316b61d2";
+
+const movie1 = {
+    Title: 'The Batman', 
+    Year: '2022', 
+    imdbID: 'tt1877830', 
+    Type: 'movie', 
+    Poster: "N/A"
+}
 
 const App = () => {
 
@@ -19,8 +28,38 @@ const App = () => {
   }, []);
 
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="app">
+      <h1>Movie City</h1>
+
+      <div className='search'>
+          <input 
+            placeholder="Search a movie"
+            value="Batman"
+            onChange={()=> {} }
+          />
+          <img  
+            src={SearchIcon} 
+            alt="search"
+            onClick={ ()=> {} }
+         />
+      </div>
+
+    <div className='container'>
+        <div className='movie'>
+           <div>
+               <p>{movie1.Year}</p>
+           </div>
+           <div>
+               <img src={movie1.Poster !=='N/A' ? movie1.Poster:"https://via.placeholder.com/400"} alt={movie1.Title}/>
+           </div>
+
+           <div>
+               <span>{movie1.Type}</span>
+               <h3>{movie1.Title}</h3>
+           </div>
+        </div>
+    </div>
+       
     </div>
   );
 }
